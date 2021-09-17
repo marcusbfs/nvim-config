6b8a78d0c29905363f9c161705f1585bd8c9dcad
Marcus Bruno <marcusbfs@gmail.com>
2021-09-17 17:10:29 -0300

    add more custom functions

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -7,9 +7,30 @@ M.mbfs_nvim_dir = function()
     vim.fn.execute(":edit init.lua")
 end
 
+-- Telescope nvim-config files
+M.mbfs_telescope_nvim_config = function()
+    require("telescope.builtin").find_files(
+        {
+            cwd = vim.fn.stdpath("config"),
+            search_dirs = {vim.fn.stdpath("config")},
+            hidden = false
+        }
+    )
+end
+
 -- Register functions
+
+local commands = {
+    {"MbfsNvimDir", "mbfs_nvim_dir"},
+    {"MbfsTelescopeNvimConfig", "mbfs_telescope_nvim_config"}
+}
+
 M.mbfs_register_functions = function()
-    vim.fn.execute(":command MbfsNvimDir lua require('core.functions').mbfs_nvim_dir()")
+    for _, item in ipairs(commands) do
+        local command = item[1]
+        local fun = item[2]
+        vim.fn.execute(":command " .. command .. " lua require('core.functions')." .. fun .. "()")
+    end
 end
 
 M.mbfs_register_functions()
