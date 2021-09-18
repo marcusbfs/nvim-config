f36b593e09affc56e5e0386ecf538ab050c5c8f1
Marcus Bruno <marcusbfs@gmail.com>
2021-09-18 16:54:02 -0300

    fix to nvim-cmp update
    Mappings were not working cause of a function called to upperase

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -34,13 +34,13 @@ cmp.setup {
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-SPACE>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
         },
-        ["<Tab>"] = function(fallback)
+        ["<TAB>"] = function(fallback)
             if vim.fn.pumvisible() == 1 then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
             elseif require("luasnip").expand_or_jumpable() then
@@ -49,7 +49,7 @@ cmp.setup {
                 fallback()
             end
         end,
-        ["<S-Tab>"] = function(fallback)
+        ["<S-TAB>"] = function(fallback)
             if vim.fn.pumvisible() == 1 then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
             elseif require("luasnip").jumpable(-1) then
