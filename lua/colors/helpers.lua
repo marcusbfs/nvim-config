cb2ff0dd4a9b515bc79f10f21bbd9e59a88a43d3
Marcus Bruno <marcusbfs@gmail.com>
2021-09-19 08:14:43 -0300

    refactor colors

---
+++ lua/colors/helpers.lua
@@ -0,0 +1,32 @@
+M = {}
+
+M.tokyonight = function(style)
+    vim.g.tokyonight_style = style
+    vim.cmd [[colorscheme tokyonight]]
+end
+
+M.gruvbox = function(background, intensity)
+    vim.o.background = background
+    vim.api.nvim_set_var("gruvbox_material_background", intensity)
+    vim.api.nvim_set_var("gruvbox_material_palette", "material")
+    vim.cmd [[colorscheme gruvbox-material]]
+end
+
+M.onedark = function(style)
+    if style ~= "default" then
+        vim.fn.execute("let g:onedark_style = '" .. style .. "'")
+    end
+    vim.cmd [[colorscheme onedark]]
+end
+
+M.ayu = function(style)
+    vim.g.ayucolor = style
+    vim.cmd [[colorscheme ayu]]
+end
+
+M.papercolor = function(style)
+    vim.o.background = style
+    vim.cmd [[colorscheme PaperColor]]
+end
+
+return M
--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,22 +1,29 @@
 -- Lua file to manage colorschemes
 
+local helpers = require("colors.helpers")
+
 -- Options
 local CS = {
-    gruvbox_material = 1,
-    tokyonight_storm = 2,
-    tokyonight_night = 3,
-    tokyonight_day = 4,
-    onedark_default = 5,
-    onedark_darker = 6,
-    onedark_warmer = 7,
-    github_default = 8,
-    github_dark = 9,
-    github_dimmed = 10,
-    github_light = 11,
-    ayu_mirage = 12,
-    ayu_dark = 13,
-    ayu_light = 14,
-    nord = 15
+    gruvbox_material_dark_medium = 1,
+    gruvbox_material_dark_hard = 2,
+    gruvbox_material_light_medium = 3,
+    gruvbox_material_light_hard = 4,
+    tokyonight_storm = 5,
+    tokyonight_night = 6,
+    tokyonight_day = 7,
+    onedark_default = 8,
+    onedark_darker = 9,
+    onedark_warmer = 10,
+    github_default = 11,
+    github_dark = 12,
+    github_dimmed = 13,
+    github_light = 14,
+    ayu_mirage = 15,
+    ayu_dark = 16,
+    ayu_light = 17,
+    nord = 18,
+    papercolor_dark = 19,
+    papercolor_light = 20
 }
 
 -- Set colorscheme
@@ -24,14 +31,14 @@ vim.g.my_colorscheme = CS.tokyonight_storm
 
 -- Set colorscheme basd on time
 local based_on_time = function()
-    local toggle_hour = 18.5
+    local toggle_hour = 17.5
     local date = os.date("*t")
     local current_time = date.hour + date.min / 59.0
 
     if current_time < toggle_hour then
         vim.g.my_colorscheme = CS.github_light
     else
-        vim.g.my_colorscheme = CS.tokyonight_storm
+        vim.g.my_colorscheme = CS.github_dark
     end
 end
 
@@ -54,7 +61,14 @@ M = {}
 M.origin_name = function()
     if cs_contains(CS.tokyonight_storm, CS.tokyonight_day, CS.tokyonight_night) then
         return "folke/tokyonight.nvim"
-    elseif cs_contains(CS.gruvbox_material) then
+    elseif
+        cs_contains(
+            CS.gruvbox_material_dark_medium,
+            CS.gruvbox_material_dark_hard,
+            CS.gruvbox_material_light_medium,
+            CS.gruvbox_material_light_hard
+        )
+     then
         return "sainnhe/gruvbox-material"
     elseif cs_contains(CS.onedark_default, CS.onedark_darker, CS.onedark_warmer) then
         return "navarasu/onedark.nvim"
@@ -64,6 +78,8 @@ M.origin_name = function()
         return "ayu-theme/ayu-vim"
     elseif cs_contains(CS.nord) then
         return "arcticicestudio/nord-vim"
+    elseif cs_contains(CS.papercolor_dark, CS.papercolor_light) then
+        return "NLKNguyen/papercolor-theme"
     else
         error("origin_name: colorscheme " .. vim.g.my_colorscheme .. " is not valid")
     end
@@ -71,27 +87,25 @@ end
 
 M.config = function()
     if cs_contains(CS.tokyonight_storm) then
-        vim.g.tokyonight_style = "storm"
-        vim.cmd [[colorscheme tokyonight]]
+        helpers.tokyonight("storm")
     elseif cs_contains(CS.tokyonight_night) then
-        vim.g.tokyonight_style = "night"
-        vim.cmd [[colorscheme tokyonight]]
+        helpers.tokyonight("night")
     elseif cs_contains(CS.tokyonight_day) then
-        vim.g.tokyonight_style = "day"
-        vim.cmd [[colorscheme tokyonight]]
-    elseif cs_contains(CS.gruvbox_material) then
-        vim.o.background = "dark"
-        vim.api.nvim_set_var("gruvbox_material_background", "medium")
-        vim.api.nvim_set_var("gruvbox_material_palette", "original")
-        vim.fn.execute("colorscheme gruvbox-material")
+        helpers.tokyonight("day")
+    elseif cs_contains(CS.gruvbox_material_dark_medium) then
+        helpers.gruvbox("dark", "medium")
+    elseif cs_contains(CS.gruvbox_material_dark_hard) then
+        helpers.gruvbox("dark", "hard")
+    elseif cs_contains(CS.gruvbox_material_light_medium) then
+        helpers.gruvbox("light", "medium")
+    elseif cs_contains(CS.gruvbox_material_light_hard) then
+        helpers.gruvbox("light", "hard")
     elseif cs_contains(CS.onedark_default) then
-        vim.cmd [[colorscheme onedark]]
+        helpers.onedark("default")
     elseif cs_contains(CS.onedark_darker) then
-        vim.fn.execute("let g:onedark_style = 'darker'")
-        vim.cmd [[colorscheme onedark]]
+        helpers.onedark("darker")
     elseif cs_contains(CS.onedark_warmer) then
-        vim.fn.execute("let g:onedark_style = 'warmer'")
-        vim.cmd [[colorscheme onedark]]
+        helpers.onedark("warmer")
     elseif cs_contains(CS.github_default) then
         require("github-theme").setup()
     elseif cs_contains(CS.github_dark) then
@@ -101,16 +115,17 @@ M.config = function()
     elseif cs_contains(CS.github_light) then
         require("github-theme").setup({theme_style = "light_default"})
     elseif cs_contains(CS.ayu_dark) then
-        vim.g.ayucolor = "dark"
-        vim.cmd [[colorscheme ayu]]
+        helpers.ayu("dark")
     elseif cs_contains(CS.ayu_light) then
-        vim.g.ayucolor = "light"
-        vim.cmd [[colorscheme ayu]]
+        helpers.ayu("light")
     elseif cs_contains(CS.ayu_mirage) then
-        vim.g.ayucolor = "mirage"
-        vim.cmd [[colorscheme ayu]]
+        helpers.ayu("mirage")
     elseif cs_contains(CS.nord) then
         vim.cmd [[colorscheme nord]]
+    elseif cs_contains(CS.papercolor_dark) then
+        helpers.papercolor("dark")
+    elseif cs_contains(CS.papercolor_light) then
+        helpers.papercolor("light")
     else
         error("config: colorscheme " .. vim.g.my_colorscheme .. " is not valid")
     end
