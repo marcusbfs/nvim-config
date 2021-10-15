42b8444896a0e4c4b9a9efce6465d17e77bec755
Marcus Bruno <marcusbfs@gmail.com>
2021-10-15 08:01:58 -0300

    fix autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,12 +1,17 @@
 vim.cmd [[ augroup MBFS ]]
 vim.cmd [[ autocmd! ]]
+
 -- Don't show any numbers inside terminals
-vim.cmd [[ au TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]]
+vim.cmd [[ autocmd TermOpen term://* setlocal nonumber norelativenumber | setfiletype terminal ]]
+
 --- Trim whitespace after saving a file
 vim.cmd [[ autocmd BufWritePre * lua require("core.functions").mbfs_trim_whitespace() ]]
+
 -- Set textwidth for markdown
-vim.cmd [[ au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=300} ]]
--- vim.cmd [[ au TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false} ]]
+vim.cmd [[ autocmd bufreadpre *.md setlocal textwidth=88 ]]
+
 -- Highlight on yank
-vim.cmd [[ autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank("IncSearch", 1000) ]]
+-- vim.cmd [[ autocmd TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false} ]]
+vim.cmd [[ autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=300} ]]
+
 vim.cmd [[ augroup END ]]
