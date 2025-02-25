0741b8ca9f327dcb56a878fbd8f6f6dbd9cc1a85
Marcus Fernandes <marcusbfs@gmail.com>
2025-02-25 14:28:14 -0300

    Re-add harpoon

--- init.lua
+++ init.lua
@@ -275,6 +275,30 @@ require("lazy").setup({
 		end,
 	},
 
+	-- Getting you where you want with the fewest keystrokes.
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<m-h><m-m>", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<m-h><m-l>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			-- Set <space>1..<space>5 be my shortcuts to moving to the files
+			for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+				vim.keymap.set("n", string.format("<space>%d", idx), function()
+					harpoon:list():select(idx)
+				end)
+			end
+		end,
+	},
+
 	-- Intelligently reopens files at your last edit position.
 	{
 		"farmergreg/vim-lastplace",
