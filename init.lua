4ee2bedaa1fb77116c2ea46d50257810eb54a646
Marcus Fernandes <marcusbfs@gmail.com>
2025-03-24 07:41:31 -0300

    Update kickstart base to `2abcb39`

--- init.lua
+++ init.lua
@@ -78,6 +78,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 2
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -480,8 +485,8 @@ require("lazy").setup({
 			},
 		},
 	is_windows and {} or {
-			"lambdalisue/vim-suda",
-		},
+		"lambdalisue/vim-suda",
+	},
 
 	-- lazy.nvim
 	-- A collection of small QoL plugins for Neovim.
