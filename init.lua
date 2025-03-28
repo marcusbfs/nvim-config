8d1ca3442e02658479f4a7ef8be0753d2ef3d721
Marcus Fernandes <marcusbfs@gmail.com>
2025-03-28 07:54:44 -0300

    Update kickstart base to `e947649`

--- init.lua
+++ init.lua
@@ -1226,16 +1226,14 @@ require("lazy").setup({
 				-- languages here or re-enable it for the disabled ones.
 				-- local disable_filetypes = { c = true, cpp = true }
 				local disable_filetypes = {}
-				local lsp_format_opt
 				if disable_filetypes[vim.bo[bufnr].filetype] then
-					lsp_format_opt = "never"
+					return nil
 				else
-					lsp_format_opt = "fallback"
+					return {
+						timeout_ms = 500,
+						lsp_format = "fallback",
+					}
 				end
-				return {
-					timeout_ms = 500,
-					lsp_format = lsp_format_opt,
-				}
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
