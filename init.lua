999009a5cb5f594c3934e7a2962ea14c53501707
Marcus Fernandes <marcusbfs@gmail.com>
2025-07-15 15:37:44 -0300

    f fold

--- init.lua
+++ init.lua
@@ -49,6 +49,10 @@ vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 
+-- f fold
+vim.opt.foldenable = false
+vim.opt.foldmethod = "manual"
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
